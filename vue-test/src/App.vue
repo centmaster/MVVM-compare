288e3c18edbf57e6a80b8d73c80cd585b4a9fc0b
centmaster <861676131@qq.com>
2017-08-08 16:33:57 +0800

    show me the code

---
+++ vue-test/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <h3>{{count}}</h3>
+    <button v-on:click="addcount()">click for add count</button>
+    <button v-on:click="addunusedcount()">click for add unusedcount</button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'This is Father',
+      count:0,
+      unusedcount:0
+    }
+  },
+  methods:{
+      addcount(){
+         this.count++;
+      },
+      addunusedcount(){
+          this.unusedcount++;
+          console.log('unused-count:',this.unusedcount);
+          console.log('unusedcount更新，但dom没有更新')
+      }
+  },
+  updated:()=>{
+      console.log('father has updated')
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-test/src/components/Hello.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>{{count}}</h3>
+    <button @click="addcount()">click for add count</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'This is child',
+      count:0
+    }
+  },
+  methods:{
+    addcount(count){
+      this.count++;
+    }
+  },
+  updated:()=>{
+    console.log('child has updated')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
