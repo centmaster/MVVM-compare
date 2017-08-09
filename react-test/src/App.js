d053235d3680415f79365012d7564dc29ee7bde3
centmaster <861676131@qq.com>
2017-08-09 21:55:34 +0800

    react vue

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -3,19 +3,94 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+ constructor(props){
+     super(props);
+     this.state={
+         fcount:0
+     }
+ }
+ fAdd=()=>{
+     this.setState({
+         fcount:++this.state.fcount
+     })
+ }
+ componentDidUpdate(){
+     console.log('father component update')
+ }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Test</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>father count:{this.state.fcount}</h2>
+          <button onClick={this.fAdd}>add father count</button>
+          <Child></Child>
+          <UpdateChild></UpdateChild>
       </div>
     );
   }
+}
+
+class Child extends Component{
+    state={
+        ccount:0
+    }
+
+    cAdd=()=>{
+        this.setState({
+            ccount:++this.state.ccount
+        })
+    }
+
+    componentDidUpdate(){
+        console.log('child1 component update')
+    }
+
+    render(){
+        return (
+        <div>
+        <h2>child1 count:{this.state.ccount}</h2>
+        <button onClick={this.cAdd}>add child count</button>
+        </div>
+        )
+    }
+}
+
+class UpdateChild extends Component{
+    state={
+        ncount:0,
+
+    }
+
+    nAdd=()=>{
+        this.setState({
+            ncount:++this.state.ncount
+        })
+    }
+
+
+
+    componentDidUpdate(){
+        console.log('child2 component  update')
+    }
+
+    shouldComponentUpdate(nextProps,nextState){
+        if(nextState.Number === this.state.Number){
+            return false
+        }
+        return true
+    }
+
+    render(){
+        return (
+            <div>
+                <h2>child2 count:{this.state.ncount}</h2>
+                <button onClick={this.nAdd}>add child count</button>
+            </div>
+        )
+    }
 }
 
 export default App;
